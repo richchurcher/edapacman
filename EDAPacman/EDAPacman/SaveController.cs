82d8c8cd0825f64d87499a0e3bd68c5cb7690551
philldellow <philldellow@gmail.com>
2015-05-01 15:48:38 +1200

    Fix migrations

--- EDAPacman/EDAPacman/SaveController.cs
+++ EDAPacman/EDAPacman/SaveController.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EDAPacman.Model;
-using EDAPacman;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using EDAPacman.Model;
+//using EDAPacman;
 
-namespace EDAPacman.DAL
-{
-    public class SaveController
-    {
-        public void Process(string command, IEnumerable<string> args)
-        {
-            //Create Add model
-            Maze model = new Maze();
-            model.SaveGame(args);
+//namespace EDAPacman.DAL
+//{
+//    public class SaveController
+//    {
+//        public void Process(string command, IEnumerable<string> args)
+//        {
+//            //Create Add model
+//            Maze model = new Maze();
+//            model.SaveGame(args);
             
-            BoardView _viewBoard = new BoardView();
+//            BoardView _viewBoard = new BoardView();
 
-            model.Persist();
-            AddView view = new AddView();
-            view.Render(model);
-            //Tell Add Model to persist to db
-            //Prep message
-            //(Create a persist and prep message method on the Add model)
-            //Render view
+//            model.Persist();
+//            AddView view = new AddView();
+//            view.Render(model);
+//            //Tell Add Model to persist to db
+//            //Prep message
+//            //(Create a persist and prep message method on the Add model)
+//            //Render view
 
-        }
-    }
-}
+//        }
+//    }
+//}
