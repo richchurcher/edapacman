9f40d317c1731b804a466796dd0fd9a7c78b590d
Daniel <daniel.lewis@devacademy.co.nz>
2015-05-01 13:35:11 +1200

    controller and keys logic

---
+++ EDAPacman/EDAPacman/BoardView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDAPacman
+{
+    public class MazeView
+    {
+        public void CreateMaze(List<List<string>> maze)
+        {
+
+            for (int i = 0; i < maze.Count; i++)
+            {
+                for (int j = 0; j < maze.Count; j++)
+                {
+                    Console.WriteLine(maze[i][j]);
+                }
+                if (i == 19)
+                {
+                    Console.Write(" \n");
+                }
+                else
+                {
+                    Console.Write("\n");
+                }
+            }
+        }
+
+        public void ClearMaze()
+        {
+            Console.Clear();
+        }
+            }
+        }
+    
